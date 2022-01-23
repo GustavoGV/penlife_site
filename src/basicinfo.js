20b89917ca67c3892aed34e0c835a24de26b42ed
ccgbalarin <ccgbalarin@gmail.com>
2022-01-23 09:52:43 -0300

    penatt

--- src/basicinfo.js
+++ src/basicinfo.js
@@ -1,54 +1,101 @@
-import { Input, Form, Button, Select, Tooltip, Space, Typography } from "antd";
+import { Input, Row, Col, Form, Button, Cascader, Select, Tooltip, Space, Typography, Menu, Dropdown } from "antd";
 import { useState } from "react";
-
 
 const { Option } = Select;
 
 export default function Body () {
     const [name, setName] = useState('');
-
+    
     return <>
+    
     <Form
         layout="vertical"
     >
-        <Form.Item
-            required= '${label} é necessário'
-            name={['nomecompleto']}
-            label='Nome Completo'
-            rules={[{ required: true, message: 'Por favor, insira um nome' }]}
-        >
+        <Row>
+            <Col span={12}
+            push={6}>
+                <Form.Item
+                    required= '${label} é necessário'
+                    name={['nomecompleto']}
+                    label='Nome Completo'
+                    rules={[{ required: true, message: 'Por favor, insira um nome' }]}
+                >
 
-            <Input placeholder="Ex: Caio Balarin"></Input>
-        </Form.Item>
+                    <Input placeholder="Ex: Caio Balarin"></Input>
+                </Form.Item>
+            </Col>    
+        </Row>
+    
+        <Row>
+            <Col span={12}
+            push={6}>
+                <Form.Item
+                    required= '${label} é obrigatório'
+                    name={['email']}
+                    label='E-mail'
+                    rules={[{type:'email'},{required:true, message:'Por favor, insira um email'}]}
+                >
+                    <Input placeholder="Ex: caiobalarin@penlife.com.br"></Input>
+                    
+                </Form.Item>
+            </Col>
+        </Row>
 
-        <Form.Item
-            required= '${label} é obrigatório'
-            name={['email']}
-            label='E-mail'
-            rules={[
-                {
-                    type:'email'
-                },
-            ]}
-        >
-            <Input placeholder="Ex: caiobalarin@penlife.com.br"></Input>
-            
-        </Form.Item>
-
-        <Form.Item
-            name={['address', 'province']}
-            label="Área"
-            rules={[{ required: true, message: 'Área é necessario' }]}
-        >
-            <Select placeholder="Selecione sua área">
-            <Option value="Educação Infantil">Educação Infantil</Option>
-            <Option value="Ensino Fundamental">Ensino Fundamental</Option>
-            <Option value="Ensino Médio">Ensino Médio</Option>
-            </Select>
-        </Form.Item>
-            <Button type="primary" htmlType="submit">
-            Submit
-            </Button>
+        <Row>
+            <Col span={8}
+            push={6}>
+                <Form.Item 
+                    name={['area']}
+                    label="Área"
+                    rules={[{ required: true, message: 'Área é necessario' }]}
+                >
+                <Cascader
+                    options={[
+                    {
+                    value: 'pedagogico',
+                    label: 'Pedagógico',
+                    children: [
+                        {
+                            value: 'ensino infantil',
+                            label: 'Ensino Infantil',
+                        },
+                        {
+                            value: 'ensino fundamental',
+                            label: 'Ensino Fundamental',
+                        },
+                        {
+                            value: 'ensino medio',
+                            label: 'Ensino Médio',
+                        },
+                    ],
+                    },
+                    {
+                            value: 'administrativo',
+                            label: 'Administrativo',
+                    },
+                    {
+                            value: 'limpeza',
+                            label: 'Limpeza',
+                    },
+                    {
+                        value: 'manutencao',
+                        label: 'Manutenção',
+                    },
+                    ]
+                    }
+                    />
+                </Form.Item>
+            </Col>
+        </Row>
+        
+        <Row>
+            <Col push={6}>
+                <Button type="primary" htmlType="submit">
+                Enviar
+                </Button>
+            </Col>
+        </Row>
+        
     </Form>
     </>
 }
