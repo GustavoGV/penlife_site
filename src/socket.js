bd708e1730e1b04645884de9a50f2770d70fb1cc
ccgbalarin <94459825+ccgbalarin@users.noreply.github.com>
2022-09-13 17:15:04 -0300

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client"
 
-export const socket = io.connect('http://localhost:3042', { transports : ['websocket'] })
+export const socket = io.connect('http://170.231.232.90:3000', { transports : ['websocket'] })
 //http://18.231.106.46:3042
 
